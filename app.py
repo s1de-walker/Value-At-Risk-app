df1c08c5ddc430401b719b9b81a4b6622710f356
Sujay <s1dewalker23@gmail.com>
2025-03-10 08:40:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.title("Value at Risk")
 st.write("")
 st.write("")
 
-st.subheader("How much could I lose over a given period, with a given probability?")
+st.subheader("How much could I lose over a given period, for a given probability?")
 
 
 # Sidebar Instructions
