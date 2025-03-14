e357a82f22ac34025f1e117ef20793cea8287166
Sujay <s1dewalker23@gmail.com>
2025-03-14 14:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ if "histogram_fig" not in st.session_state:
 if "data" not in st.session_state:
     st.session_state.data = None
 
+if "hl_var_result" not in st.session_state:
+    st.session_state.hl_var_result = None
+
 # User Inputs
 stock = st.text_input("Enter Stock/ETF Symbol:", value="SPY")
 col1, col2 = st.columns(2)
